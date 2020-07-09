d76c6f4fdee8657e8f96615d0bb71b4374a09cc7
Maria Ozamiz <ozamiz.m@gmail.com>
2020-07-09 20:01:01 +0200

    add readInput function

--- src/js/01-search.js
+++ src/js/01-search.js
@@ -1,1 +1,18 @@
 'use strict';
+
+//working with search form
+
+const searchButton = document.querySelector('.js-search-form__button');
+searchButton.addEventListener('click', readInput);
+
+function readInput() {
+    event.preventDefault();
+    const searchField = document.querySelector('.js-search-form__input');
+    let inputValue = searchField.value;
+    console.log(inputValue);
+    if (inputValue) {
+        console.log('hay texto en el input');
+    } else {
+        console.log('NO hay texto en el input');
+    }
+}
