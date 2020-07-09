092eb23b5e2d24be32f2d792f4c874c1e8c899f5
Maria Ozamiz <ozamiz.m@gmail.com>
2020-07-09 20:43:33 +0200

    add makeRequest function

--- src/js/01-search.js
+++ src/js/01-search.js
@@ -1,18 +1,37 @@
 'use strict';
 
-//working with search form
+let searchResults;
 
+// action button selection and listener
 const searchButton = document.querySelector('.js-search-form__button');
 searchButton.addEventListener('click', readInput);
 
+// on button click, check and read input value
 function readInput() {
     event.preventDefault();
     const searchField = document.querySelector('.js-search-form__input');
     let inputValue = searchField.value;
-    console.log(inputValue);
     if (inputValue) {
-        console.log('hay texto en el input');
+        makeRequest();
     } else {
         console.log('NO hay texto en el input');
     }
+}
+
+// API request
+function makeRequest(inputValue) {
+    fetch(`http://api.tvmaze.com/search/shows?q=${inputValue}`)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            searchResults = data;
+            console.log(searchResults);
+        });
+    paintSearchResults();
+}
+
+// API request
+function paintSearchResults() {
+    const SearchResultsList = document.querySelector('.js-results-list');
 }
