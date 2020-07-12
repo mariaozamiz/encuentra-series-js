28679396d0f0c679f1930c7936ba3c1b785df404
Maria Ozamiz <ozamiz.m@gmail.com>
2020-07-12 15:43:55 +0200

    set-favs

--- src/js/02-set-favs.js
+++ src/js/02-set-favs.js
@@ -29,6 +29,9 @@ function paintFavShowList() {
     sectionTitle.classList.remove('hidden');
     // get favs from local storage
     const favsInfoList = JSON.parse(localStorage.getItem('favShows'));
+    //paint title
+    const sectionTitle = document.querySelector('.favs-list__title');
+    sectionTitle.classList.remove('hidden');
     // clean favs list
     const favsList = document.querySelector('.js-favs-list');
     favsList.innerHTML = '';
