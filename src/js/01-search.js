ad2768a9540fc1eec588808ebe208355225c37ab
Maria Ozamiz <ozamiz.m@gmail.com>
2020-10-23 21:08:31 +0200

    organise functions

--- src/js/01-search.js
+++ src/js/01-search.js
@@ -1,25 +1,22 @@
 'use strict';
 
-// variables
 let results = [];
 let favs = [];
 
-paintFavs();
-
-// search button listener
+// search button
 const searchButton = document.querySelector('.js-search-form__button');
-searchButton.addEventListener('click', readInput);
+searchButton.addEventListener('click', inputHandler);
 
-// read input value
-function readInput(event) {
+// get input value
+function inputHandler(event) {
     event.preventDefault();
     const searchField = document.querySelector('.js-search-form__input');
     let inputValue = searchField.value;
-    makeRequest(inputValue);
+    GetDataFromApi(inputValue);
 }
 
-// make API request and save results
-function makeRequest(inputValue) {
+// API search
+function GetDataFromApi(inputValue) {
     fetch(`http://api.tvmaze.com/search/shows?q=${inputValue}`)
         .then(function (response) {
             return response.json();
@@ -28,60 +25,4 @@ function makeRequest(inputValue) {
             results = data;
             paintResults();
         });
-}
-
-//Pain results in HTML
-function paintResults() {
-    const resultsList = document.querySelector('.js-results-list');
-    // Clear results page first
-    resultsList.innerHTML = '';
-    addSectionTitle('results');
-    // Paint card info
-    for (const result of results) {
-        let newLi = createCard(resultsList, result);
-        checkIfNeedsHighlight(newLi);
-        newLi.addEventListener('click', checkIsFav);
-    }
-}
-
-// PAINTING __________________________________________
-
-function addSectionTitle(stringlistname) {
-    const sectionTitle = document.querySelector(
-        `.${stringlistname}-list__title`
-    );
-    sectionTitle.classList.remove('hidden');
-}
-
-function createCard(parent, element) {
-    // li
-    const newLi = document.createElement('li');
-    parent.appendChild(newLi);
-    // li atribute
-    newLi.setAttribute('id', element.show.id);
-    // image
-    const newImage = document.createElement('img');
-    newLi.appendChild(newImage);
-    if (!element.show.image) {
-        newImage.src =
-            'https://via.placeholder.com/210x295/ffffff/666666/?text=TV';
-    } else {
-        newImage.src = element.show.image.medium;
-    }
-    // title
-    const newTitle = document.createElement('h3');
-    newLi.appendChild(newTitle);
-    newTitle.innerHTML = element.show.name;
-    return newLi;
-}
-
-function checkIfNeedsHighlight(newLi) {
-    // check if what is painted on results is already at favs
-    const alreadyAFav = favs.find((fav) => {
-        return fav.show.id === parseInt(newLi.id);
-    });
-    // highlight
-    if (alreadyAFav) {
-        newLi.classList.add('yellow-background');
-    }
 }
